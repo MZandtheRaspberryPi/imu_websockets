3c6fd5d061e6739457897d340fabf0c15007f2c1
mzandtheraspberrypi@gmail.com <mzandtheraspberrypi@gmail.com>
2023-09-03 16:15:02 +0200

    adding filter message and a util function for millis

--- include/util.h
+++ include/util.h
@@ -6,3 +6,10 @@
 void delay(int64_t sleep_ms) {
   std::this_thread::sleep_for(std::chrono::milliseconds(sleep_ms));
 }
+
+uint64_t get_timestamp() {
+  std::chrono::milliseconds ms =
+      std::chrono::duration_cast<std::chrono::milliseconds>(
+          std::chrono::system_clock::now().time_since_epoch());
+  return ms.count();
+}
